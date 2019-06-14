3085351e9714ed5808d9235aaa937117210333ad
omkarRJoshi <omkarjoshi9545@gmail.com>
2019-06-14 11:43:26 +0530

    new project added

--- src/com/revision/sparkStreaming/AdvancedTwitterStreaming.scala
+++ src/com/revision/sparkStreaming/AdvancedTwitterStreaming.scala
@@ -7,6 +7,7 @@ import org.apache.log4j.Level
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkConf
+import scala.io.Source
 
 
 object AdvancedTwitterStreaming {
@@ -16,30 +17,43 @@ object AdvancedTwitterStreaming {
     
     Logger.getLogger("org").setLevel(Level.ERROR)
     
-   val conf = new SparkConf().setMaster("local[*]").setAppName("Spark Streaming")
+    val conf = new SparkConf().setMaster("local[*]").setAppName("Spark Streaming")
     // here local[*] shows that spark code will run on all possible cores.
     val ssc = new StreamingContext(conf, Seconds(5))
+    
+    var consumerKey = ""
+    var consumerSecret = ""
+    var accessToken = ""
+    var accessTokenSecret = ""
+    
+    val keyFile = "/home/omkar/Desktop/twitter-setup"
+    val line = Source.fromFile(keyFile).getLines()
+    
+    for(line <- Source.fromFile(keyFile).getLines()){
+       val keys = line.split(",")
+       consumerKey = keys(0).toString()
+       consumerSecret = keys(1).toString()
+       accessToken = keys(2).toString()
+       accessTokenSecret = keys(3).toString()
+    }
      
-     val consumerKey = "AYlFUAbUgkLNuJcTK4ZlSVU4U"
-    val consumerSecret = "gf4lBLySoYUOTMTtjAuWxV2rGDvnRv0BMxdLa4M80X7VSPBdfR"
-    val accessToken = "986971830513696769-U2mYoMCfxOyDojOvn7JPbvypcgl7kmM"
-    val accessTokenSecret = "fzrqsLZA9HI7TdAPgMd5mTxXUpvougAdfcb481LdfPGvQ"
+   
     
     System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
     System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
     System.setProperty("twitter4j.oauth.accessToken", accessToken)
     System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
     
-    val filters = Array("Avengers")
+    val filters = Array("DCvsSRH", "SRHvsDC","vivoipl", "vivoipl2019")
     
-    val tweets = TwitterUtils.createStream(ssc, None, filters)
+    val tweets = TwitterUtils.createStream(ssc, None)
     
     val tweetMap = tweets.map(status => 
       
       "Location : "+status.getUser.getLocation +
+      "\nname : "+status.getUser.getName+
       "\nhashtag : "+status.getText.split(" ").filter(_.startsWith("#")).mkString(" ") +
       "\ntweet : "+status.getText+
-      "\nname : "+status.getUser.getName+
       "\nlanguage : "+status.getUser.getLang +
       "\ntime : "+status.getCreatedAt.getTime+
       "\nMail ID : "+status.getUser.getEmail+
@@ -48,9 +62,7 @@ object AdvancedTwitterStreaming {
     )
     
     tweetMap.print()
-    
-    
-    
+      
     ssc.start()
     ssc.awaitTermination()
     
